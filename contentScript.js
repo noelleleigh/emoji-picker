c81685dc50a25c7f28401f71ef48823d2aa6ea3c
Noelle Leigh <noelle@apps.directemployers.org>
2020-05-10 11:42:53 -0400

    remove logging

--- contentScript.js
+++ contentScript.js
@@ -4,14 +4,14 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.emoji) {
     const emoji = request.emoji;
     const activeElement = document.activeElement;
-    console.log(emoji);
-    console.log(activeElement);
 
     if (
       activeElement.tagName === "INPUT" &&
       ["text", "search"].includes(activeElement.type)
     ) {
       activeElement.value += request.emoji;
+    } else {
+      console.error(`Don't know how to write to ${activeElement.tagName}`);
     }
   }
 });
